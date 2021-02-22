7a8d556ca6ed7cda1a619651c62e377da018157b
VanDai <dai.phan@vmodev.com>
2021-02-22 16:48:50 +0700

    Fix issue: load profile always  when launch app

--- app/src/main/java/com/clearkeep/db/clear_keep/dao/UserDAO.kt
+++ app/src/main/java/com/clearkeep/db/clear_keep/dao/UserDAO.kt
@@ -10,9 +10,6 @@ interface UserDao {
     @Insert(onConflict = REPLACE)
     suspend fun save(user: User)
 
-    @Query("SELECT * FROM user WHERE user_name =:userName LIMIT 1")
-    suspend fun getUserByName(userName: String): User?
-
-    @Query("SELECT * FROM user")
-    fun getUser(): LiveData<User>
+    @Query("SELECT * FROM user LIMIT 1")
+    suspend fun getUser(): User?
 }
