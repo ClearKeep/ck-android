6d363a7e91c23e2389a7969dc84b9f21baa148d6
VanDai <dai.phan@vmodev.com>
2021-03-19 10:52:47 +0700

    Fix UI as SRS: register and login screen

--- app/src/main/java/com/clearkeep/components/base/CKTextField.kt
+++ app/src/main/java/com/clearkeep/components/base/CKTextField.kt
@@ -1,62 +1,86 @@
 package com.clearkeep.components.base
 
+import androidx.compose.foundation.clickable
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
-import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.runtime.*
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
-import com.clearkeep.utilities.printlnCK
 
 @Composable
 fun CKTextField(
-        label: String,
-        placeholder: String,
-        textValue: MutableState<String>,
-        modifier: Modifier = Modifier,
-        keyboardType: KeyboardType = KeyboardType.Text,
-        passwordVisibility: Boolean = false,
-        error: String? = null
+    label: String,
+    placeholder: String,
+    textValue: MutableState<String>,
+    modifier: Modifier = Modifier,
+    keyboardType: KeyboardType = KeyboardType.Text,
+    error: String? = null
 ) {
+    val isPasswordType = keyboardType == KeyboardType.Password
+    var passwordVisibility = remember { mutableStateOf(false) }
     Column {
-            OutlinedTextField(
-                    value = textValue.value,
-                    onValueChange = { textValue.value = it },
-                    label = {
-                            Text(label, style = MaterialTheme.typography.body2.copy(
-                                    color = Color.Gray
-                            ))
-                    },
-                    placeholder = { Text(placeholder, style = MaterialTheme.typography.body2.copy(
-                            color = Color.Gray))
-                    },
-                    colors = TextFieldDefaults.outlinedTextFieldColors(
-                            cursorColor = Color.Red,
-                            focusedBorderColor = Color.Red,
-                            unfocusedBorderColor = Color.Blue,
-                    ),
-                    textStyle = MaterialTheme.typography.body2.copy(
-                            color = Color.Blue
-                    ),
-                    keyboardOptions = KeyboardOptions(
-                            keyboardType = keyboardType
-                    ),
-                    visualTransformation = if (!passwordVisibility) VisualTransformation.None else PasswordVisualTransformation(),
-                    modifier = modifier.fillMaxWidth(),
-            )
-            if (!error.isNullOrEmpty()) {
-                    Spacer(modifier = Modifier.height(5.dp))
-            }
-            if (!error.isNullOrEmpty()) Text(error,
-                    style = MaterialTheme.typography.body2.copy(color = Color.Red),
-                    modifier = Modifier.padding(start = 8.dp)
-            )
+        OutlinedTextField(
+            value = textValue.value,
+            onValueChange = { textValue.value = it },
+            label = {
+                Text(
+                    label, style = MaterialTheme.typography.body2.copy(
+                        color = Color.Gray
+                    )
+                )
+            },
+            placeholder = {
+                Text(
+                    placeholder, style = MaterialTheme.typography.body2.copy(
+                        color = Color.Gray
+                    )
+                )
+            },
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                cursorColor = Color.Red,
+                focusedBorderColor = Color.Red,
+                unfocusedBorderColor = Color.Blue,
+            ),
+            textStyle = MaterialTheme.typography.body2.copy(
+                color = Color.Blue
+            ),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = keyboardType
+            ),
+            visualTransformation = if (isPasswordType) {
+                if (passwordVisibility.value) VisualTransformation.None else PasswordVisualTransformation()
+            } else {
+                VisualTransformation.None
+            },
+            modifier = modifier.fillMaxWidth(),
+            trailingIcon = {
+                if (isPasswordType) {
+                    Icon(
+                        imageVector = if (passwordVisibility.value) Icons.Filled.Visibility else Icons.Filled.VisibilityOff,
+                        contentDescription = "",
+                        tint = if (passwordVisibility.value) Color.Blue else Color.Gray,
+                        modifier = Modifier.clickable(
+                            onClick = { passwordVisibility.value = !passwordVisibility.value }
+                        )
+                    )
+                }
+            },
+        )
+        if (!error.isNullOrEmpty()) {
+            Spacer(modifier = Modifier.height(5.dp))
+        }
+        if (!error.isNullOrEmpty()) Text(
+            error,
+            style = MaterialTheme.typography.body2.copy(color = Color.Red),
+            modifier = Modifier.padding(start = 8.dp)
+        )
     }
 }
