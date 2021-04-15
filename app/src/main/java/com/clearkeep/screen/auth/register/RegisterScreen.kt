e91f31f207a1130ff0560941ef66926fba218d08
VanDai <dai.phan@vmodev.com>
2021-04-15 11:40:04 +0700

    update color and icon

--- app/src/main/java/com/clearkeep/screen/auth/register/RegisterScreen.kt
+++ app/src/main/java/com/clearkeep/screen/auth/register/RegisterScreen.kt
@@ -2,6 +2,7 @@ package com.clearkeep.screen.auth.register
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.*
@@ -22,7 +23,9 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.clearkeep.R
 import com.clearkeep.components.base.CKButton
+import com.clearkeep.components.base.CKTextButton
 import com.clearkeep.components.base.CKTextInputField
+import com.clearkeep.components.base.TextButtonType
 import com.clearkeep.components.grayscaleBlack
 import com.clearkeep.components.primaryDefault
 
@@ -45,116 +48,112 @@ fun RegisterScreen(
 
     val image = painterResource(R.drawable.ic_logo)
 
-    Column(modifier = Modifier.fillMaxSize()) {
+    Column(
+        modifier = Modifier
+            .padding(horizontal = 20.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(contentAlignment = Alignment.TopCenter) {
+            Image(image, contentDescription = "")
+        }
+        Spacer(Modifier.height(24.dp))
+        Card(shape = RoundedCornerShape(16.dp)) {
+            Column(
+                modifier = Modifier
+                    .background(Color.White)
+                    .padding(horizontal = 16.dp, vertical = 24.dp)
+            ) {
+                Text(
+                    text = stringResource(R.string.sign_up_fill_information),
+                    color = grayscaleBlack
+                )
 
-        Column(
-            modifier = Modifier
-                .padding(horizontal = 20.dp)
-                .fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Spacer(Modifier.height(24.dp))
-
-            Box(contentAlignment = Alignment.TopCenter) {
-                Image(image, contentDescription = "")
-            }
-            Spacer(Modifier.height(24.dp))
-            Card(shape = RoundedCornerShape(16.dp)) {
-                Column(
-                    modifier = Modifier
-                        .background(Color.White)
-                        .padding(horizontal = 16.dp, vertical = 24.dp)
-                ) {
-                    Text(
-                        text = stringResource(R.string.sign_up_fill_information),
-                        color = grayscaleBlack
-                    )
-
-                    CKTextInputField(
-                        "Email",
-                        email,
-                        keyboardType = KeyboardType.Email,
-                        error = emailError.value,
-                        singleLine = true,
-                        leadingIcon = {
-                            Image(
-                                painterResource(R.drawable.ic_icon_mail),
-                                contentDescription = null
-                            )
-                        }
-                    )
-                    Spacer(Modifier.height(10.dp))
-                    CKTextInputField(
-                        "Display name",
-                        displayName,
-                        error = displayNameError.value,
-                        singleLine = true,
-                        leadingIcon = {
-                            Image(
-                                painterResource(R.drawable.ic_user_check),
-                                contentDescription = null
-                            )
-                        }
-
-                    )
-                    Spacer(Modifier.height(10.dp))
-                    CKTextInputField(
-                        "Password",
-                        password,
-                        keyboardType = KeyboardType.Password,
-                        error = passError.value,
-                        singleLine = true,
-                        leadingIcon = {
-                            Image(
-                                painterResource(R.drawable.ic_icon_lock),
-                                contentDescription = null
-                            )
-                        }
-                    )
-                    Spacer(Modifier.height(10.dp))
-                    CKTextInputField(
-                        "Confirm password",
-                        confirmPassword,
-                        keyboardType = KeyboardType.Password,
-                        error = confirmPassError.value,
-                        singleLine = true,
-                        leadingIcon = {
-                            Image(
-                                painterResource(R.drawable.ic_icon_lock),
-                                contentDescription = null
-                            )
-                        }
-                    )
-                    Spacer(Modifier.height(24.dp))
-                    Row(verticalAlignment = Alignment.CenterVertically) {
-                        Text(
-                            text = "Sign in instead",
-                            modifier = Modifier.weight(1.0f, true),
-                            style = TextStyle(
-                                color = primaryDefault,
-                                fontWeight = FontWeight.Bold,
-                                fontSize = 12.sp
-                            )
-                        )
-
-                        CKButton(
-                            stringResource(R.string.sign_up),
-                            onClick = {
-                                onRegisterPressed(
-                                    email.value,
-                                    displayName.value,
-                                    password.value,
-                                    confirmPassword.value
-                                )
-                            },
-                            enabled = !isLoading,
-                            modifier = Modifier
-                                .width(120.dp)
-                                .height(40.dp)
+                CKTextInputField(
+                    "Email",
+                    email,
+                    keyboardType = KeyboardType.Email,
+                    error = emailError.value,
+                    singleLine = true,
+                    leadingIcon = {
+                        Image(
+                            painterResource(R.drawable.ic_icon_mail),
+                            contentDescription = null
                         )
                     }
-                    Spacer(Modifier.height(30.dp))
+                )
+                Spacer(Modifier.height(10.dp))
+                CKTextInputField(
+                    "Display name",
+                    displayName,
+                    error = displayNameError.value,
+                    singleLine = true,
+                    leadingIcon = {
+                        Image(
+                            painterResource(R.drawable.ic_user_check),
+                            contentDescription = null
+                        )
+                    }
+
+                )
+                Spacer(Modifier.height(10.dp))
+                CKTextInputField(
+                    "Password",
+                    password,
+                    keyboardType = KeyboardType.Password,
+                    error = passError.value,
+                    singleLine = true,
+                    leadingIcon = {
+                        Image(
+                            painterResource(R.drawable.ic_icon_lock),
+                            contentDescription = null
+                        )
+                    }
+                )
+                Spacer(Modifier.height(10.dp))
+                CKTextInputField(
+                    "Confirm password",
+                    confirmPassword,
+                    keyboardType = KeyboardType.Password,
+                    error = confirmPassError.value,
+                    singleLine = true,
+                    leadingIcon = {
+                        Image(
+                            painterResource(R.drawable.ic_icon_lock),
+                            contentDescription = null
+                        )
+                    }
+                )
+                Spacer(Modifier.height(24.dp))
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Row(
+                        modifier = Modifier
+                            .weight(1.0f, true),
+                        horizontalArrangement = Arrangement.Start
+                    ) {
+                        CKTextButton(
+                            title = "Sign in instead",
+                            onClick = onBackPress,
+                            textButtonType = TextButtonType.Blue
+                        )
+                    }
+
+                    CKButton(
+                        stringResource(R.string.sign_up),
+                        onClick = {
+                            onRegisterPressed(
+                                email.value,
+                                displayName.value,
+                                password.value,
+                                confirmPassword.value
+                            )
+                        },
+                        enabled = !isLoading,
+                        modifier = Modifier
+                            .width(120.dp)
+                            .height(40.dp)
+                    )
                 }
             }
         }
