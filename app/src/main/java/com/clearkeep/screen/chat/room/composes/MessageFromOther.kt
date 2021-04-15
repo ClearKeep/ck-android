26b9ab658e9ad9eb54bc5d4237e31bd78db9c1d5
VanDai <dai.phan@vmodev.com>
2021-04-15 14:59:41 +0700

    refactor message display shape

--- app/src/main/java/com/clearkeep/screen/chat/room/composes/MessageFromOther.kt
+++ app/src/main/java/com/clearkeep/screen/chat/room/composes/MessageFromOther.kt
@@ -1,37 +1,38 @@
 package com.clearkeep.screen.chat.room.composes
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.clearkeep.components.colorSuccessDefault
 import com.clearkeep.components.grayscale3
 import com.clearkeep.components.grayscaleOffWhite
 import com.clearkeep.components.primaryDefault
-import com.clearkeep.db.clear_keep.model.Message
+import com.clearkeep.screen.chat.room.message_display_generator.MessageDisplayInfo
 import com.clearkeep.utilities.getHourTimeAsString
 
 @Composable
-fun MessageFromOther(message: Message, username: String, isGroup: Boolean) {
+fun MessageFromOther(messageDisplayInfo: MessageDisplayInfo) {
     Column(modifier = Modifier.fillMaxWidth()) {
-        if (isGroup) {
+        if (messageDisplayInfo.showAvatarAndName) {
             Row(
                 modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceAround
             ) {
                 Text(
-                    text = username,
-                    style = MaterialTheme.typography.caption,
+                    text = messageDisplayInfo.userName,
+                    style = MaterialTheme.typography.caption.copy(
+                        color = colorSuccessDefault,
+                    ),
                 )
                 Text(
-                    text = getHourTimeAsString(message.createdTime),
+                    text = getHourTimeAsString(messageDisplayInfo.message.createdTime),
                     style = MaterialTheme.typography.caption.copy(
                         fontWeight = FontWeight.Medium,
                         color = grayscale3,
@@ -45,19 +46,26 @@ fun MessageFromOther(message: Message, username: String, isGroup: Boolean) {
             Box(
                 modifier = Modifier.weight(2.0f, true),
             ) {
-                RoundCornerMessage(
-                    message = message.message,
+                Card (
                     backgroundColor = primaryDefault,
-                    textColor = grayscaleOffWhite,
-                )
+                    shape = messageDisplayInfo.cornerShape,
+                ) {
+                    Text(
+                        text = messageDisplayInfo.message.message,
+                        style = MaterialTheme.typography.body2.copy(
+                            color = grayscaleOffWhite
+                        ),
+                        modifier = Modifier.padding(horizontal = 24.dp, vertical = 8.dp)
+                    )
+                }
             }
             Column(
                 modifier = Modifier.weight(1.0f, true),
                 horizontalAlignment = Alignment.End
             ) {
-                if (!isGroup) {
+                if (!messageDisplayInfo.showAvatarAndName) {
                     Text(
-                        text = getHourTimeAsString(message.createdTime),
+                        text = getHourTimeAsString(messageDisplayInfo.message.createdTime),
                         style = MaterialTheme.typography.caption.copy(
                             fontWeight = FontWeight.Medium,
                             color = grayscale3,
@@ -67,26 +75,5 @@ fun MessageFromOther(message: Message, username: String, isGroup: Boolean) {
                 }
             }
         }
-    }
-}
-
-@Composable
-private fun RoundCornerMessage(message: String, backgroundColor: Color, textColor: Color) {
-    Card (
-        backgroundColor = backgroundColor,
-        shape = RoundedCornerShape(
-            topStart = 0.dp,
-            topEnd = roundSize,
-            bottomEnd = roundSize,
-            bottomStart = roundSize
-        ),
-    ) {
-        Text(
-            text = message,
-            style = MaterialTheme.typography.body2.copy(
-                color = textColor
-            ),
-            modifier = Modifier.padding(horizontal = 24.dp, vertical = 8.dp)
-        )
     }
 }
