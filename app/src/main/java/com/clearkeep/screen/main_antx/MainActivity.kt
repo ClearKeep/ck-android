5dd7c0bf65cd1d21134c37c40a3d73bd4db5d41f
antx <v.antx3@vinsmart.net.vn>
2021-04-27 09:19:32 +0700

    demo menu ui

--- app/src/main/java/com/clearkeep/screen/main_antx/MainActivity.kt
+++ app/src/main/java/com/clearkeep/screen/main_antx/MainActivity.kt
@@ -1,13 +1,11 @@
-package com.clearkeep.screen.main
+package com.clearkeep.screen.main_antx
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -15,8 +13,6 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModelProvider
 import com.clearkeep.components.CKTheme
-import com.clearkeep.components.grayscale2
-import com.clearkeep.components.grayscaleBackground
 import com.clearkeep.components.grayscaleOffWhite
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
