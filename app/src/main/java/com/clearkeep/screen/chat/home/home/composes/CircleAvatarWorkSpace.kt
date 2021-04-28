4e9e745babb052d0e8677d46c8f57231898bef0f
antx <v.antx3@vinsmart.net.vn>
2021-04-28 17:13:08 +0700

    update ui

--- app/src/main/java/com/clearkeep/screen/chat/home/home/composes/CircleAvatarWorkSpace.kt
+++ app/src/main/java/com/clearkeep/screen/chat/home/home/composes/CircleAvatarWorkSpace.kt
@@ -1,11 +1,9 @@
 package com.clearkeep.screen.chat.home.home.composes
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -14,36 +12,19 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.clearkeep.components.backgroundGradientEnd
 import com.clearkeep.components.backgroundGradientStart
-import com.clearkeep.components.grayscale2
-import com.clearkeep.components.primaryDefault
 import com.clearkeep.db.clear_keep.model.Server
 
 
 @Composable
-fun CircleAvatarWorkSpace(item: Server, idLive: Long?) {
+fun CircleAvatarWorkSpace(item: Server, idWorkSpaceLive: Long?) {
     val displayName =
         if (item.serverName.isNotBlank() && item.serverName.length >= 2) item.serverName.substring(0, 2) else item.serverName
-    Column(modifier = if (idLive == item.id) Modifier
-        .size(60.dp)
-        .background(shape = CircleShape,color = Color.Transparent)
-        .border(2.dp, Color.Magenta,shape = CircleShape)
-
-    else Modifier.height(56.dp).width(56.dp)
-        .background(
-            shape = CircleShape,
-            color = Color.White
-        )
-        ,
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally) {
     Surface(
         shape = CircleShape,
-        modifier = Modifier
-            .size(48.dp)
+        modifier = Modifier.size(48.dp)
     ) {
         Column(
             modifier = Modifier.background(
@@ -58,10 +39,12 @@ fun CircleAvatarWorkSpace(item: Server, idLive: Long?) {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Text(displayName.capitalize(), style = MaterialTheme.typography.caption.copy(
-                color = MaterialTheme.colors.onSurface,
-            ))
+            Text(
+                displayName.capitalize(), style = MaterialTheme.typography.caption.copy(
+                    color = MaterialTheme.colors.onSurface,
+                )
+            )
         }
-    }
+
     }
 }
