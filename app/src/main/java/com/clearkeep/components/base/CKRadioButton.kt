98eec5cba926c823be4401f4f7ad550e07e4a242
Phan Văn Đại <dai.phan@vmodev.com>
2021-04-28 11:20:31 +0700

    update UI for create group

--- app/src/main/java/com/clearkeep/components/base/CKRadioButton.kt
+++ app/src/main/java/com/clearkeep/components/base/CKRadioButton.kt
@@ -1,10 +1,17 @@
 package com.clearkeep.components.base
 
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.RadioButton
-import androidx.compose.material.RadioButtonDefaults
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.clearkeep.R
 import com.clearkeep.components.grayscale4
 
 @Composable
@@ -14,14 +21,34 @@ fun CKRadioButton(
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
 ) {
-    RadioButton(
-        colors = RadioButtonDefaults.colors(
-            selectedColor = MaterialTheme.colors.primary,
-            unselectedColor = grayscale4,
-        ),
-        selected = selected,
-        onClick = onClick,
-        modifier = modifier,
-        enabled = enabled
+    val colors = RadioButtonDefaults.colors(
+        selectedColor = MaterialTheme.colors.primary,
+        unselectedColor = grayscale4,
     )
+    val radioColor by colors.radioColor(enabled, selected)
+    Surface(
+        shape = CircleShape,
+        modifier = modifier
+            .size(RadioButtonSize).clickable {
+                if (onClick != null) {
+                    onClick()
+                }
+            },
+        color = radioColor
+    ) {
+        if (selected) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.ic_done),
+                    contentDescription = null,
+                    tint = Color.White
+                )
+            }
+        }
+    }
 }
+
+private val RadioButtonSize = 32.dp
