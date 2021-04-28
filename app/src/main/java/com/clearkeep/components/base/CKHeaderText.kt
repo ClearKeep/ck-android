a71458b370a49ebd9d5df8b43b7c99f33c75d748
Phan Văn Đại <dai.phan@vmodev.com>
2021-04-28 15:18:35 +0700

    update theme

---
+++ app/src/main/java/com/clearkeep/components/base/CKHeaderText.kt
@@ -0,0 +1,40 @@
+package com.clearkeep.components.base
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import com.clearkeep.components.grayscaleBlack
+import com.clearkeep.components.grayscaleOffWhite
+
+@Composable
+fun CKHeaderText(
+    text: String,
+    modifier: Modifier = Modifier,
+    headerTextType: HeaderTextType = HeaderTextType.Normal
+) {
+    Text(
+        text = text,
+        modifier = modifier,
+        style = getTypography(headerTextType).copy(
+            color = if (isSystemInDarkTheme()) grayscaleOffWhite else grayscaleBlack
+        )
+    )
+}
+
+@Composable
+fun getTypography(headerTextType: HeaderTextType): TextStyle {
+    return when (headerTextType) {
+        HeaderTextType.Normal -> MaterialTheme.typography.h6
+        HeaderTextType.Medium -> MaterialTheme.typography.h5
+        HeaderTextType.Large -> MaterialTheme.typography.h4
+    }
+}
+
+enum class HeaderTextType {
+    Normal,
+    Medium,
+    Large
+}
