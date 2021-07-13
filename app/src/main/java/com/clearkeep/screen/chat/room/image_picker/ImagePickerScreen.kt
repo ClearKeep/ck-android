37d19c5906e40d31d3b6d22fd44154d230b481ee
Van Linh Nguyen <linhnv@vmodev.com>
2021-07-13 11:04:00 +0700

    Pick image feature

--- app/src/main/java/com/clearkeep/screen/chat/room/image_picker/ImagePickerScreen.kt
+++ app/src/main/java/com/clearkeep/screen/chat/room/image_picker/ImagePickerScreen.kt
@@ -10,6 +10,8 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
@@ -20,9 +22,8 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -38,36 +39,58 @@ import com.clearkeep.R
 import com.clearkeep.components.CKSimpleTheme
 import com.clearkeep.components.base.CKRadioButton
 import com.clearkeep.components.base.CKTopAppBar
+import com.clearkeep.components.primaryDefault
+import com.clearkeep.db.clear_keep.model.User
+import com.clearkeep.screen.chat.room.RoomViewModel
 import com.clearkeep.utilities.printlnCK
 import com.google.accompanist.glide.rememberGlidePainter
 
 @ExperimentalFoundationApi
 @Composable
 fun ImagePickerScreen(
+    roomViewModel: RoomViewModel,
     navController: NavController
 ) {
     val context = LocalContext.current
+    val urisSelected = remember { roomViewModel.imageUriSelected.value?.toMutableStateList() ?: mutableStateListOf() }
 
     val uris = getAllImages(context)
     printlnCK(uris.toString())
 
     CKSimpleTheme {
         Column {
-            CKTopAppBar({}, Modifier, navigationIcon = {
-                IconButton(onClick = {
-                    navController.popBackStack()
-                }) {
-                    Icon(painterResource(R.drawable.ic_cross), null)
-                }
-            }, actions = { Text("Upload ()", modifier = Modifier.padding(end = 16.dp)) })
-
+            printlnCK((roomViewModel.imageUriSelected.value?.size ?: 0).toString())
+            CKTopAppBar(
+                {},
+                Modifier,
+                navigationIcon = {
+                    IconButton(onClick = {
+                        navController.popBackStack()
+                    }) {
+                        Icon(painterResource(R.drawable.ic_cross), null)
+                    }
+                },
+                actions = {
+                    Text(
+                        "Upload (${urisSelected?.size ?: 0})",
+                        modifier = Modifier
+                            .padding(end = 16.dp)
+                            .clickable {
+                                roomViewModel.setSelectedImages(urisSelected)
+                                navController.popBackStack()
+                            }
+                    )
+                })
             Box(Modifier.padding(16.dp)) {
                 LazyVerticalGrid(cells = GridCells.Fixed(2)) {
                     items(uris) {
                         ImageItem(
-                            Modifier.fillParentMaxWidth(0.5f), it.toString(), true
-                        ) {
-
+                            Modifier.fillParentMaxWidth(0.5f),
+                            it.toString(),
+                            urisSelected?.contains(it.toString())
+                        ) { uri: String, isSelected: Boolean ->
+                            if (isSelected)
+                                urisSelected.add(uri) else urisSelected.remove(uri)
                         }
                     }
                 }
@@ -107,12 +130,21 @@ fun getAllImages(context: Context): List<Uri> {
 }
 
 @Composable
-fun ImageItem(modifier: Modifier, uri: String, isSelected: Boolean, onSelect: (String) -> Unit) {
+fun ImageItem(
+    modifier: Modifier,
+    uri: String,
+    isSelected: Boolean,
+    onSelect: (String, Boolean) -> Unit
+) {
     Box(
         modifier.then(
             Modifier
                 .clip(RectangleShape)
                 .aspectRatio(1f)
+                .clickable {
+                    onSelect(uri, !isSelected)
+                }
+                .then(if (isSelected) Modifier.border(2.dp, primaryDefault) else Modifier)
         )
     ) {
         Image(
@@ -121,19 +153,15 @@ fun ImageItem(modifier: Modifier, uri: String, isSelected: Boolean, onSelect: (S
             contentScale = ContentScale.Crop,
             modifier = modifier
         )
-        CKRadioButton(
-            selected = isSelected,
-            onClick = { onSelect(uri) },
+        Box(
             Modifier
                 .align(Alignment.BottomEnd)
                 .padding(bottom = 12.dp, end = 12.dp)
-        )
+        ) {
+            Image(
+                painter = painterResource(id = if (isSelected) R.drawable.ic_checkbox else R.drawable.ic_ellipse_20),
+                "",
+            )
+        }
     }
-}
-
-@ExperimentalFoundationApi
-@Preview
-@Composable
-fun ImagePickerScreenPreview() {
-    ImageItem(Modifier, "", false) {}
 }
