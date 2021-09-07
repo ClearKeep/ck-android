26739d43e0b6ac5be24bd43f2d8242a65d0ed482
Van Linh Nguyen <linhnv@vmodev.com>
2021-09-07 17:34:28 +0700

    UI for search

--- app/src/main/java/com/clearkeep/screen/chat/contact_search/SearchViewModel.kt
+++ app/src/main/java/com/clearkeep/screen/chat/contact_search/SearchViewModel.kt
@@ -2,6 +2,7 @@ package com.clearkeep.screen.chat.contact_search
 
 import androidx.lifecycle.*
 import com.clearkeep.db.clear_keep.model.User
+import com.clearkeep.screen.chat.repo.GroupRepository
 import com.clearkeep.screen.chat.repo.PeopleRepository
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
@@ -10,7 +11,8 @@ import javax.inject.Inject
 
 class SearchViewModel @Inject constructor(
     private val peopleRepository: PeopleRepository,
-) : ViewModel() {
+    private val roomRepository: GroupRepository,
+    ) : ViewModel() {
     private val debouncePeriod: Long = 1000
 
     private var searchJob: Job? = null
@@ -19,7 +21,6 @@ class SearchViewModel @Inject constructor(
     val isShowLoading:MutableLiveData<Boolean> = MutableLiveData()
 
     val friends: LiveData<List<User>> get() = _friends
-
 
     init {
         search("")
