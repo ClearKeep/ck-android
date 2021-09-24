a4a4c624829ade992d3d74afe43466c40e8ef05e
Van Linh Nguyen <linhnv@vmodev.com>
2021-09-24 17:49:32 +0700

    Add loading status to social login flow

--- app/src/main/java/com/clearkeep/screen/chat/social_login/ConfirmSocialLoginPhraseScreen.kt
+++ app/src/main/java/com/clearkeep/screen/chat/social_login/ConfirmSocialLoginPhraseScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -27,6 +28,7 @@ fun ConfirmSocialLoginPhraseScreen(viewModel: LoginViewModel, onBackPress: () ->
     val securityPhrase = remember { mutableStateOf("") }
     val isSecurityPhraseValid = viewModel.isConfirmSecurityPhraseValid.observeAsState()
     val registerResponse = viewModel.registerSocialPinResponse.observeAsState()
+    val isLoading = viewModel.isLoading.observeAsState()
 
     BackHandler {
         onBackPress()
@@ -36,62 +38,56 @@ fun ConfirmSocialLoginPhraseScreen(viewModel: LoginViewModel, onBackPress: () ->
         onLoginSuccess()
     }
 
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .verticalScroll(rememberScrollState())
-    ) {
-        Spacer(Modifier.height(58.sdp()))
-        CKTopAppBarSample(title = "Confirm Your Security Phrase", onBackPressed = { onBackPress() })
-        Spacer(Modifier.height(30.sdp()))
-
-        Column(
-            modifier = Modifier
-                .padding(horizontal = 16.sdp())
-                .fillMaxSize(),
+    Box(Modifier.fillMaxSize()) {
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
         ) {
-            CKText(
-                text = "Remember this security phrase. If you forget it, you’ll need to reset your account and all data will be erased.",
-                style = MaterialTheme.typography.caption,
-                color = grayscaleOffWhite,
-                fontSize = 16.sdp().toNonScalableTextSize()
-            )
-            Spacer(Modifier.height(32.sdp()))
-            CKTextInputField(
-                "Confirm Security Phrase",
-                securityPhrase, singleLine = true,
-                leadingIcon = {
-                    Image(
-                        painterResource(R.drawable.ic_icon_lock),
-                        contentDescription = null
-                    )
-                },
-                keyboardType = KeyboardType.Password,
-                error = if (isSecurityPhraseValid.value == true || securityPhrase.value.isBlank()) null else "Security phrase and confirm security phrase do not match. Please try again"
-            ) {
-                viewModel.setConfirmSecurityPhrase(it)
-            }
-            Spacer(Modifier.height(104.sdp()))
-            CKButton(
-                stringResource(R.string.btn_next),
-                onClick = {
-                    viewModel.onSubmitNewPin()
-                },
-                modifier = Modifier.fillMaxWidth(),
-                enabled = isSecurityPhraseValid.value == true,
-                buttonType = ButtonType.White
-            )
+            Spacer(Modifier.height(58.sdp()))
+            CKTopAppBarSample(title = "Confirm Your Security Phrase", onBackPressed = { onBackPress() })
+            Spacer(Modifier.height(30.sdp()))
 
-//            if (isEmptyEmail.value || isInvalidEmailFormat.value) {
-//                CKAlertDialog(
-//                    title = if (isEmptyEmail.value) stringResource(R.string.email_blank_error) else stringResource(
-//                        R.string.email_invalid),
-//                    text = stringResource(R.string.pls_check_again),
-//                    onDismissButtonClick = {
-//                        isEmptyEmail.value = false
-//                        isInvalidEmailFormat.value = false
-//                    }
-//                )
-//            }
+            Column(
+                modifier = Modifier
+                    .padding(horizontal = 16.sdp())
+                    .fillMaxSize(),
+            ) {
+                CKText(
+                    text = "Remember this security phrase. If you forget it, you’ll need to reset your account and all data will be erased.",
+                    style = MaterialTheme.typography.caption,
+                    color = grayscaleOffWhite,
+                    fontSize = 16.sdp().toNonScalableTextSize()
+                )
+                Spacer(Modifier.height(32.sdp()))
+                CKTextInputField(
+                    "Confirm Security Phrase",
+                    securityPhrase, singleLine = true,
+                    leadingIcon = {
+                        Image(
+                            painterResource(R.drawable.ic_icon_lock),
+                            contentDescription = null
+                        )
+                    },
+                    keyboardType = KeyboardType.Password,
+                    error = if (isSecurityPhraseValid.value == true || securityPhrase.value.isBlank()) null else "Security phrase and confirm security phrase do not match. Please try again"
+                ) {
+                    viewModel.setConfirmSecurityPhrase(it)
+                }
+                Spacer(Modifier.height(104.sdp()))
+                CKButton(
+                    stringResource(R.string.btn_next),
+                    onClick = {
+                        viewModel.onSubmitNewPin()
+                    },
+                    modifier = Modifier.fillMaxWidth(),
+                    enabled = isSecurityPhraseValid.value == true && isLoading.value == false,
+                    buttonType = ButtonType.White
+                )
+            }
+        }
+
+        if (isLoading.value == true) {
+            CKCircularProgressIndicator(Modifier.align(Alignment.Center))
         }
     }
 }
