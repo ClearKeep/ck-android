2640bde6165353b3c99b88d5522fc7d74a3a8156
Van Linh Nguyen <linhnv@vmodev.com>
2021-10-14 09:51:51 +0700

    Fix call UI when user leave call

--- app/src/main/java/com/clearkeep/screen/videojanus/surface_generator/SurfacePosition.kt
+++ app/src/main/java/com/clearkeep/screen/videojanus/surface_generator/SurfacePosition.kt
@@ -1,6 +1,6 @@
 package com.clearkeep.screen.videojanus.surface_generator
 
-class SurfacePosition(
+data class SurfacePosition(
     val width: Int,
     val height: Int,
     val marginStart: Int,
