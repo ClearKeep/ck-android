8f814ce28442d63dbcc207ae6a1ce853b10a4fb8
Van Linh Nguyen <linhnv@vmodev.com>
2021-10-27 16:47:45 +0700

    Change upload file to HTTP

--- app/src/main/java/com/clearkeep/MyApplication.kt
+++ app/src/main/java/com/clearkeep/MyApplication.kt
@@ -1,7 +1,43 @@
 package com.clearkeep
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import dagger.hilt.android.HiltAndroidApp
+import net.gotev.uploadservice.UploadServiceConfig
 
 @HiltAndroidApp
-open class MyApplication : Application()
+open class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        createNotificationChannel()
+
+        UploadServiceConfig.initialize(
+            context = this,
+            defaultNotificationChannel = notificationChannelID,
+            debug = BuildConfig.DEBUG
+        )
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= 26) {
+            val channel = NotificationChannel(
+                notificationChannelID,
+                "TestApp Channel",
+                NotificationManager.IMPORTANCE_LOW
+            )
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
+        }
+    }
+
+    companion object {
+        // ID of the notification channel used by upload service. This is needed by Android API 26+
+        // but you have to always specify it even if targeting lower versions, because it's handled
+        // by AndroidX AppCompat library automatically
+        const val notificationChannelID = "TestChannel"
+    }
+}
