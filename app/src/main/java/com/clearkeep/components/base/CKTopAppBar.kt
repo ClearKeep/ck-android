e47698571701c776e3666f93f1e8a10b6d40cad7
Van Linh Nguyen <linhnv@vmodev.com>
2021-11-10 10:17:59 +0700

    Dark theme for chat screen

--- app/src/main/java/com/clearkeep/components/base/CKTopAppBar.kt
+++ app/src/main/java/com/clearkeep/components/base/CKTopAppBar.kt
@@ -1,11 +1,15 @@
 package com.clearkeep.components.base
 
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material.AppBarDefaults
 import androidx.compose.material.TopAppBar
-import androidx.compose.material.primarySurface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.clearkeep.components.LocalColorMapping
+import com.clearkeep.components.grayscaleDarkModeDarkGrey2
+import com.clearkeep.components.primaryDefault
+import com.clearkeep.utilities.printlnCK
 
 @Composable
 fun CKTopAppBar(
@@ -13,12 +17,18 @@ fun CKTopAppBar(
     modifier: Modifier = Modifier,
     navigationIcon: @Composable (() -> Unit)? = null,
     actions: @Composable RowScope.() -> Unit = {},
+    isDarkTheme: Boolean = false
 ) {
     TopAppBar(
         title = title,
         modifier = modifier,
         navigationIcon = navigationIcon,
         actions = actions,
-        backgroundColor = MaterialTheme.colors.primary,
+        backgroundColor = if (isDarkTheme) {
+            grayscaleDarkModeDarkGrey2
+        } else {
+            primaryDefault
+        },
+        elevation = 0.dp
     )
 }
