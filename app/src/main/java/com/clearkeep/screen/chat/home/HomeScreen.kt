c9eb34c0848e91f86372732bbdc724e83df13ea2
TuNAVMO <tuna@vmodev.com>
2022-07-05 09:56:03 +0700

    CLK32 - fixed Enter space at first/last of server url and email: not trim space

--- app/src/main/java/com/clearkeep/screen/chat/home/HomeScreen.kt
+++ app/src/main/java/com/clearkeep/screen/chat/home/HomeScreen.kt
@@ -536,6 +536,7 @@ fun JoinServerComposable(
                 rememberServerUrl,
                 keyboardType = KeyboardType.Text,
                 singleLine = true,
+                allowSpace = false
             )
             Spacer(modifier = Modifier.size(14.sdp()))
             CKButton(
