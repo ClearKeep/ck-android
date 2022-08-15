e9f97c935a1e46f78db3d2909748f05b9f6531a0
Alex Viet <alex.viet@vmodev.com>
2022-08-15 15:30:55 +0700

    update reset password

--- domain/src/main/java/com/clearkeep/domain/usecase/auth/LoginUseCase.kt
+++ domain/src/main/java/com/clearkeep/domain/usecase/auth/LoginUseCase.kt
@@ -126,9 +126,11 @@ class LoginUseCase @Inject constructor(
         domain: String,
         rawNewPassword: String
     ): Resource<AuthRes> {
+        Log.d("antx: ", "LoginUseCase resetPassword line = 129:$preAccessToken \n$email \n$domain \n$rawNewPassword " );
         val (saltHex, verificatorHex) = createAccountSrp(email, rawNewPassword)
-
+        Log.d("antx: ", "LoginUseCase resetPassword line = 131:$saltHex $verificatorHex" );
         val decrypter = DecryptsPBKDF2(rawNewPassword)
+        Log.d("antx: ", "LoginUseCase resetPassword line = 133: decrypter: $decrypter" );
         // create registrationId
         val registrationId = KeyHelper.generateRegistrationId(false)
         //PreKeyRecord
@@ -156,8 +158,8 @@ class LoginUseCase @Inject constructor(
             signedPreKey.signature,
             preAccessToken = preAccessToken,
             email,
-            saltHex,
             verificatorHex,
+            saltHex,
             DecryptsPBKDF2.toHex(decrypter.getIv()),
             domain
         )
